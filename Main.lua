2381e2eb1d8e841114b94166405662647f43c7d1
Dasunerwartete <126102661+Dasunerwartete@users.noreply.github.com>
2025-04-05 18:58:05 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,7 @@
 print("Hello, World!!!")
+
+local ModuleScript = Instance.new("ModuleScript")
+ModuleScript.Parent = game:GetService("Workspace")
+ModuleScript.Source = [[
+print("Hello from dynamically created module!")
+]]
